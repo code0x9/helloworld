45586790212b39f0055b1b4a9fa52ff9b4fc91b5
Niko Bellic <niko.bellic@daumkakao.com>
2015-08-27 18:47:02 +0900

    bump up

--- hello.go
+++ hello.go
@@ -26,7 +26,7 @@ import (
 func main() {
 	hostname, _ := os.Hostname()
 	phase := os.Getenv("PHASE")
-	version := 3
+	version := 4
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("hello there")
