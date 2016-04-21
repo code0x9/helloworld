c3210c2ad953d4d63ccead448ac3859304a5f5e9
Niko Bellic <niko.bellic@kakaocorp.com>
2016-04-21 16:29:27 +0900

    bump up

--- bumpup.sh
+++ bumpup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-### date modified: 2016년 4월 21일 목요일 15시 31분 10초 KST/
+### date modified: 2016년 4월 21일 목요일 16시 29분 27초 KST/
 sed -i "2 d" $0
 sed -i "2 i ### date modified: $(date)/" $0
 
 VER=$(cat VERSION)
 echo $((VER+1)) > VERSION
 
-#git commit -a -m "bump up"
-#git push
+git commit -a -m "bump up"
+git push
