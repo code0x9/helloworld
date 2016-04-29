db28cf4b1a5d8ce468ec99bdc82758f059cdd091
Niko Bellic <niko.bellic@kakaocorp.com>
2016-04-29 16:16:27 +0900

    bump up

--- bumpup.sh
+++ bumpup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-### date modified: 2016년 4월 22일 금요일 17시 32분 30초 KST/
+### date modified: 2016년 4월 29일 금요일 16시 16분 27초 KST/
 sed -i "2 d" $0
 sed -i "2 i ### date modified: $(date)/" $0
 
