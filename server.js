d0b96903e0cd168575db6ada0bc3ea003b51c6bf
Niko Bellic <niko.bellic@kakaocorp.com>
2016-11-28 15:33:19 +0900

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const port = 8080
+const version = 40
+let http = require('http')
+
+http.createServer(function(request, response) {
+    console.log(`${new Date().toISOString()} ${request.connection.remoteAddress} ${request.url} `)
+    response.end(`Hello World! version:${version}\n`)
+}).listen(port)
+
+console.log(`listening on port ${port}`)
