e18029e64f44e4bea685524e15e65002be4a4fb0
Mark Lee <code0x9@gmail.com>
2016-12-09 17:40:33 +0900

    bumpup to 2

--- bumpup.sh
+++ bumpup.sh
@@ -4,3 +4,6 @@ VER=`grep "$VERSION_STRING" server.js | awk '{print $NF}'`
 NEW_VER=$(($VER + 1))
 echo "current version: $VER, new version: $NEW_VER"
 sed -i "s/$VERSION_STRING $VER$/$VERSION_STRING $NEW_VER/" server.js
+
+git commit -am "bumpup to $NEW_VER"
+git push public master
