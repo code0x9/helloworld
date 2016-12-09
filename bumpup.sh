2f111c98cc2ec50365083944d3459818703780ce
Niko Bellic <niko.bellic@kakaocorp.com>
2016-12-09 17:38:00 +0900

    added bumpup script

---
+++ bumpup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+VERSION_STRING="const version ="
+VER=`grep "$VERSION_STRING" server.js | awk '{print $NF}'`
+NEW_VER=$(($VER + 1))
+echo "current version: $VER, new version: $NEW_VER"
+sed -i "s/$VERSION_STRING $VER$/$VERSION_STRING $NEW_VER/" server.js
